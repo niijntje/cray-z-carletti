a8dad6e4fb410f7b7db6de6377f141dc5081bb8f
cederdorff <cederdorff@gmail.com>
2011-12-01 08:45:13 +0000

    Opdateringer i adminFrames

--- CarlettiV0/gui/SubFrameAdminMellemlagerPlads.java
+++ CarlettiV0/gui/SubFrameAdminMellemlagerPlads.java
@@ -37,7 +37,7 @@ public class SubFrameAdminMellemlagerPlads extends JFrame {
 	 * Create the frame.
 	 */
 	public SubFrameAdminMellemlagerPlads() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 400, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.PINK);
