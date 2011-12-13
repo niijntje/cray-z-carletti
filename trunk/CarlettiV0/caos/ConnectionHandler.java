1ce34ac85c825a3cee24aec4290d4b3c5ced7647
niijntje@gmail.com <niijntje@gmail.com@1092542a-29a2-a500-3b2e-e2aafb5a5309>
2011-12-13 07:53:52 +0000

    Min måde at håndtere connections og opgaveløsning ;-)

---
+++ trunk/CarlettiV0/caos/ConnectionHandler.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package caos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author nijntje
+ *
+ */
+public class ConnectionHandler {
+	
+	private Connection myConnection;
+	private static ConnectionHandler uniqueInstance;
+	
+	private ConnectionHandler(){
+
+	}
+	
+	public static ConnectionHandler getInstance(){
+		if (uniqueInstance == null){
+			uniqueInstance = new ConnectionHandler();
+		}
+		return uniqueInstance;
+	}
+	
+	public Connection getConnection(){
+		Connection myConnection = null;
+		try {
+			// connection to MSSQL
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+			myConnection = DriverManager.getConnection(
+					"jdbc:jtds:sqlserver://192.168.1.106/master", "sa", "hemmeligtPassword");
+//			"jdbc:jtds:sqlserver://10.77.44.115/master", "sa", "hemmeligtPassword");
+
+			
+
+		} catch (Exception e) {
+			System.out.println("Error connecting to database:  "
+					+ e.getMessage());
+		}
+		return myConnection;
+	}
+	
+	public ResultSet getSQLResult(String sqlQuery){
+		
+		ResultSet resultSet = null;
+		try {
+			// connection to MSSQL
+			Class.forName("net.sourceforge.jtds.jdbc.Driver");
+			myConnection = DriverManager.getConnection(
+					"jdbc:jtds:sqlserver://192.168.1.106/master", "sa", "hemmeligtPassword");
+//			"jdbc:jtds:sqlserver://10.77.44.115/master", "sa", "hemmeligtPassword");
+
+			Statement stmt = myConnection.createStatement();
+
+			resultSet = stmt.executeQuery(sqlQuery);
+			
+			
+
+		} catch (Exception e) {
+			System.out.println("Error connecting to database:  "
+					+ e.getMessage());
+		}
+
+		return resultSet;
+	}
+	
+	public void closeConnection() throws SQLException{
+		myConnection.close();
+	}
+	
+	
+
+}
